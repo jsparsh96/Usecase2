f84d3360dd8f6729bc9773228aba55b41852da43
jsparsh96 <60638737+jsparsh96@users.noreply.github.com>
2020-03-23 19:58:18 +0530

    Add files via upload
    Committed after completion

---
+++ src/usecase2-app/home-page.js
@@ -0,0 +1,140 @@
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-form/iron-form.js';
+import '@polymer/paper-card/paper-card.js';
+import '@polymer/polymer/lib/elements/dom-repeat.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+
+/**
+* @customElement
+* @polymer
+*/
+class HomePage extends PolymerElement {
+    static get template() {
+        return html`
+<style>
+    main {
+        border-radius: 8px;
+        align-self: center;
+    }
+
+    iron-form{
+        padding:20px;
+    }
+    h1 {
+        justify-self: center;
+        color: white;
+    }
+
+    header {
+        display: grid;
+        grid-template-columns: 250px 1fr 1fr 200px 100px;
+        grid-template-rows: 100px;
+        height: 100px;
+        width: 100%;
+        background-color: #000000;
+    }
+    #logoutBtn{
+        margin-left:20%;
+        margin-top:30%;
+    }
+    #logout {
+
+        grid-row: 1/2;
+        grid-column: 5/6;
+    }
+
+    #userName {
+        grid-row: 1/2;
+        grid-column: 4/5;
+        color:white;
+        margin-top:10%;
+    }
+
+    #heading {
+        margin: 10px;
+        font-size: 1.4em;
+        grid-row: 1/2;
+        grid-column: 1/2;
+    }
+    paper-card{
+        background-color: rgb(216, 211, 211);
+
+        margin-top:2%;
+        width:100%;
+    }
+    iron-icon {
+        color: red;
+    }
+</style>
+<header>
+    <div id="heading">
+        <h1>ShopHere<iron-icon icon="shopping-cart"></iron-icon>
+        </h1>
+    </div>
+    <div id="userName">
+        <h2>Welcome, {{user.customerName}}</h2>
+    </div>
+    <div id="logout">
+        <paper-button id="logoutBtn" on-click="_handleLogout">
+            <iron-icon icon="power-settings-new"></iron-icon>
+        </paper-button>
+    </div>
+</header>
+
+    <main>
+    <template is="dom-repeat" items="{{products}}">
+    <paper-card>
+    <h3>Product Name: {{item.productName}}</h3>
+    <h3>Product Description: {{item.description}}</h3>
+    <h3>Product Price: {{item.price}}</h3>
+  </paper-card>
+  </template>
+    </main>
+
+<iron-ajax id="ajax" on-response="_handleResponse" on-error="_handleError" content-type="application/json"
+    handle-as="json"></iron-ajax>
+
+`;
+    }
+    static get properties() {
+        return {
+            prop1: {
+                type: String,
+                value: 'login-page'
+            },
+            products:{
+                type:Array,
+                value:[]
+            }
+        };
+    }
+   connectedCallback(){
+       super.connectedCallback();
+       this.user = JSON.parse(sessionStorage.getItem('userDetails'));
+       this._makeAjaxCall(`http://localhost:3000/products?priority=${this.user.customerType}`,`get`,null)
+   }
+    _handleLogout() {
+        sessionStorage.clear();
+        window.history.pushState({}, null, '#/login');
+        window.dispatchEvent(new CustomEvent('location-changed'));
+        window.location.reload();
+
+    }
+    // _handleError() {
+    // }
+    _handleResponse(event) {
+        this.products = event.detail.response;
+    }
+    _makeAjaxCall(url, method, postObj, action) {
+    const ajax = this.$.ajax;
+    ajax.method = method;
+    ajax.url = url;
+    ajax.body = postObj ? JSON.stringify(postObj) : undefined;
+    ajax.generateRequest();
+    }
+}
+
+window.customElements.define('home-page', HomePage);
---
+++ src/usecase2-app/usecase2-app.js
@@ -0,0 +1,87 @@
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
+import '@polymer/app-route/app-location.js';
+import '@polymer/app-route/app-route.js';
+import '@polymer/iron-pages/iron-pages.js';
+
+setPassiveTouchGestures(true);
+setRootPath(MyAppGlobals.rootPath);
+/**
+ * @customElement
+ * @polymer
+ */
+class Usecase2App extends PolymerElement {
+  static get template() {
+    return html`
+    <app-location route="{{route}}" url-space-regex="^[[rootPath]]" use-hash-as-path></app-location>
+    <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}"></app-route>     
+    <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+      <registration-page name="registration"></registration-page>
+      <login-page name="login"></login-page>
+      <home-page balance={{balance}} name="home"></home-page>
+      <view404-page name='view404'></view404-page>
+    </iron-pages>
+    `;
+  }
+  static get properties() {
+    return {
+      page: {
+        type: String,
+        reflectToAttribute: true,
+        observer: '_pageChanged'
+      },
+      routeData: Object,
+      subroute: Object
+    };
+  }
+
+  static get observers() {
+    return [
+      '_routePageChanged(routeData.page)'
+    ];
+  }
+  ready(){
+    super.ready();
+  }
+
+
+
+  /**
+  * 
+  * @param {String} page 
+  */
+  _routePageChanged(page) {
+    console.log(page)
+    if (!page) {
+      this.page = 'login';
+    } else if (['registration', 'home', 'login'].indexOf(page) !== -1) {
+      this.page = page;
+    } else {
+      this.page = 'view404';
+    }
+  }
+
+  /**
+   * 
+   * @param {String} page 
+   */
+  _pageChanged(page) {
+    console.log(page)
+    switch (page) {
+      case 'registration':
+        import('./registration-page.js');
+        break;
+      case 'home':
+        import('./home-page.js')
+        break;
+      case 'login':
+        import('./login-page.js');
+        break;
+      case 'view404':
+        import('./view404-page.js');
+        break;
+    }
+  }
+}
+
+window.customElements.define('usecase2-app', Usecase2App);
